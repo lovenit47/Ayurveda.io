b1ccb62bdc3acd6f65479954d958d4f14520d5fe
lovenit47 <lovenit.oceanicmedia@gmail.com>
2023-10-12 16:53:45 +0530

    res

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
+// if else 
 
 
+// let hasvoterid = 'no';
+// let age = 17;
+
+// if ( hasvoterid == 'yes' && age >= 18){
+//     alert("yes you can vote!");
+// }else if ( hasvoterid == 'no' && age == 18){
+//     alert("Get your voterID!");
+// }else{
+//     alert("you can't vote");
+// }
+
+
+// turnery operators
+
+
+// let user = 0;
+
+// let option = user==1 ? alert('hello') : alert ('bye');
+
+
+// neloscolising 
+
+// let user;
+// user;
+
+// alert(user?? 'guest');
+
+// Loop while loop --- sum of all even 1 to 100
+
+// let counter = 0;
+// let sum = 0;
+
+// while (counter <= 100){
+//     if(counter % 2 == 0){
+//         sum = sum + counter;
+//     }
+//     counter++;
+// }
+// document.write(sum);
+
+
+// let counter2 = 0;
+// let sum2 = 0;
+
+// while (counter2 <= 100){
+  
+//    sum2 = sum2 +counter2;
+//    counter2 = counter2 + 2;
+// }
+// document.write(sum2);
+
+
+let type3 ="five" *  "5";
+ console.log(typeof type3);
+
+ 
